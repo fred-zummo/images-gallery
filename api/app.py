3fc2869e097d0a773095a1639cf3d57c9acec305
FZummo <fred.zummo@outlook.com>
2021-12-01 14:37:50 -0500

    added GET request to unsplash api

--- api/app.py
+++ api/app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, request
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY")
+
+if not UNSPLASH_KEY:
+  raise EnvironmentError("Required UNSPLASH_KEY missing from .env.local file")
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-  return "Hello World!"
+@app.route("/new-image")
+def new_image():
+  word = request.args.get("query")
+  headers = {
+    "Authorization": "Client-ID " + UNSPLASH_KEY 
+  }
+  params = { "query" : word }
+  response=requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+  data = response.json()
+  return data
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5050)
