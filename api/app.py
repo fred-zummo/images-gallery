b795eda4f043241d0aefeb300b9ea1abb040d9d4
FZummo <fred.zummo@outlook.com>
2021-12-04 15:26:50 -0500

    implemented basic images endpoint with GET and POST methods

--- api/app.py
+++ api/app.py
@@ -1,8 +1,12 @@
 import os
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -34,6 +38,20 @@ def new_image():
   response=requests.get(url=UNSPLASH_URL, headers=headers, params=params)
   data = response.json()
   return data
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+  if request.method == "GET":
+    # read images from the db
+    images = images_collection.find({})
+    return jsonify([img for img in images])
+  if request.method == "POST":
+    # save image in the db
+    image = request.get_json()
+    image["_id"] = image.get("id")
+    result = images_collection.insert_one(image)
+    inserted_id = result.inserted_id
+    return {"inserted_id": inserted_id}
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5050)
